9f5c771b7aa5fd5edad016dd6ad81b97aefdfa69
Julian Lau <julian.lau@aol.com>
2022-02-16 22:15:03 +0100

    pwa config

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    pwa: {
+      name: 'Alphabet Typer',
+      themeColor: '#2A2D34',
+      msTileColor: '#2A2D34',
+      navigationColor: "#2A2D34",
+      navigationColorDark: "#2A2D34",
+      navigationDividerColor: "#2A2D34",
+      navigationDividerColorDark: "#2A2D34",
+      backgroundColor: '#2A2D34',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'default',
+    },
+  }
+  
