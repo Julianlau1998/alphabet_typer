bc617843dce0f0191afa648566d1d1ab3fb30d6a
julianlau1998 <Julian.lau@aol.com>
2022-11-02 21:33:29 +0100

    ios

--- src/components/game/Game.vue
+++ src/components/game/Game.vue
@@ -165,7 +165,7 @@ export default {
             'recordModule'
         ]),
         iOS () {
-          return window.webkit
+          return window.webkit && window.webkit.messageHandlers.toggleMessageHandler
         },
         fetchedRecords () {
             return this.recordModule.records.data
